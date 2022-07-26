9b3cec3b33e858118bd300cbeaa0a97da16b9dfa
Asan Azimkulov <azimkulov40@gmail.com>
2022-07-26 19:42:00 +0600

    Убирает папку /js с продакшена

--- js/components/white-popup.js
+++
@@ -1,5 +0,0 @@
-$('.close-white-popup').on('click', function () {
-  $('body').removeClass('overlay');
-  $('.white-popup-wrapper').addClass('d-none');
-  $('.navbar').removeClass('navbar-hidden');
-})
